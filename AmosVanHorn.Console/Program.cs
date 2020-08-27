ee59f834294164ac4a3194bbfe94e67f935b3da8
Amos Van Horn <amos.vanhorn@gmail.com>
2020-08-27 15:23:25 -0500

    final commit
    final commit

--- AmosVanHorn.Console/Program.cs
+++ AmosVanHorn.Console/Program.cs
@@ -11,11 +11,16 @@ namespace HelloWorldAmosVanHorn
     {
         static void Main(string[] args)
         {
+            // create message writer service
             var messageWriterService = new MessageWriterService();
+            
+            // write message (letting service use the default writer, which is specified in app.config)
             messageWriterService.WriteMessage("Hello World");
 
+            // write press to exit message
             Console.WriteLine("\npress any key to exit...");
 
+            // wait for user keypress before closing
             Console.ReadKey();
         }
     }
